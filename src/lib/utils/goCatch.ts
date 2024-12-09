3f70dcfb23e5a31edf64308bb260eb0b6b53b929
branila <stefan.branila55@gmail.com>
2024-12-09 22:32:04 +0100

    improved documentation

---
+++ src/lib/utils/goCatch.ts
@@ -0,0 +1,58 @@
+/**
+ * Handles a promise and captures errors, returning the error or the result in a tuple.
+ *
+ * This utility function helps manage promises by avoiding the need for try-catch blocks.
+ * It captures errors and returns them in a consistent format, optionally filtering
+ * for specific error types. Unexpected errors can be re-thrown.
+ *
+ * @template T - The type of the resolved value of the promise.
+ * @template E - The type of the error class to catch, extending the `Error` class.
+ *
+ * @param {Promise<T>} promise - The promise to handle.
+ * @param {E[] | E} [errorsToCatch] - An optional value that can be either a single error constructor
+ * or an array of error constructors. If provided, only errors matching these constructors will be captured.
+ * Other errors are re-thrown.
+ *
+ * @returns {Promise<[undefined, T] | [InstanceType<E>, undefined]>} A promise that resolves to a tuple.
+ * - `[undefined, T]` if the promise resolves successfully.
+ * - `[InstanceType<E>, undefined]` if the promise is rejected with an error matching `errorsToCatch`.
+ *
+ * @throws If the promise is rejected with an error not listed in `errorsToCatch`, the error is re-thrown.
+ *
+ * @example
+ * const [error, data] = await goCatch(promise)
+ * if (error) {
+ *   console.error('An error occurred:', error)
+ * }
+ */
+export default async function goCatch<
+  T, E extends new (message?: string) => Error
+>(
+  promise: Promise<T>,
+  errorsToCatch?: E[] | E
+): Promise<[undefined, T] | [InstanceType<E>, undefined]> {
+  try {
+      const data = await promise
+
+      // If the promise resolves successfully, return the data
+      return [undefined, data] as [undefined, T]
+  } catch (error) {
+    if (!errorsToCatch) {
+      // If no error types are provided, return the error
+      return [error, undefined] as [InstanceType<E>, undefined]
+    }
+
+    // If a single error type is provided, convert it to an array
+    if (!Array.isArray(errorsToCatch)) {
+      errorsToCatch = [errorsToCatch]
+    }
+
+    // If the error matches one of the provided error types, return the error
+    if (Array.isArray(errorsToCatch) && errorsToCatch.some((E) => error instanceof E)) {
+      return [error, undefined] as [InstanceType<E>, undefined]
+    }
+
+    // If the error does not match any of the provided error types, re-throw the error
+    throw error
+  }
+}
