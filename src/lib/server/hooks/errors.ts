985ee12e8f36294440d099dd88559769608e7c99
branila <stefan.branila55@gmail.com>
2024-12-10 18:40:19 +0100

    implemented notification system for unexpected errors

---
+++ src/lib/server/hooks/errors.ts
@@ -0,0 +1,17 @@
+import notify from '$lib/utils/notify'
+import { type HandleServerError } from '@sveltejs/kit'
+
+export const errorsHandler: HandleServerError = async ({ error, status, event }) => {
+  const user = event.locals.user?.email || 'Unauthenticated User'
+  const err = (error as Error).stack || 'Unknown Error'
+
+  const msg = `User: ${user}\n${err}`
+
+  console.error(msg)
+
+  if (status !== 404) {
+    await notify(msg)
+  }
+}
+
+export default errorsHandler
