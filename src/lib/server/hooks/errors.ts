ae5ad13271e504d80cd1754a6cf06d70ea37d167
branila <stefan.branila55@gmail.com>
2024-12-15 00:46:36 +0100

    minor changes to error handler hook due to my autism

--- src/lib/server/hooks/errors.ts
+++ src/lib/server/hooks/errors.ts
@@ -2,9 +2,10 @@ import notify from '$lib/utils/notify'
 import {type HandleServerError} from '@sveltejs/kit'
 
 export const errorsHandler: HandleServerError = async ({ error, status, event }) => {
-  const user = event.locals.user?.email || 'guest'
+  const user = event.locals.user?.email || 'Guest'
+  const err = false|| new Error('Unknown Error')
 
-  const msg = `User: ${user}\n\n${error}`
+  const msg = `User: ${user}\n\n${err.stack}`
 
   console.error(msg)
 
