8c0c1458cfd316f6ba6f8837489d9c0abc52e1e9
branila <stefan.branila55@gmail.com>
2024-12-17 00:07:25 +0100

    minor change

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,28 +1,28 @@
 <script lang="ts">
-  import PocketBase from "pocketbase";
+  import PocketBase from 'pocketbase'
 
-  const p = $props();
+  const { data } = $props()
 
   async function auth() {
-    const pb = new PocketBase(p.data.pb_instance);
+    const pb = new PocketBase(data.pb_instance)
 
-    const authData = await pb.collection("users").authWithOAuth2({
-      provider: "oidc",
-    });
+    const authData = await pb.collection('users').authWithOAuth2({
+      provider: 'oidc',
+    })
 
-    pb.authStore.save(authData.token, authData.record);
+    pb.authStore.save(authData.token, authData.record)
 
     document.cookie = pb.authStore.exportToCookie({
       httpOnly: false,
       secure: true,
-      sameSite: "strict",
-    });
+      sameSite: 'strict',
+    })
 
-    window.location.href = "/cogestione";
+    window.location.href = '/cogestione'
   }
 </script>
 
-Login
+Login <br>
 <button onclick={auth}>Accedi</button>
 
 <style>
