5f5f941d04036a6fc312a523e2a7415bb9cd51d3
branila <stefan.branila55@gmail.com>
2024-12-17 00:09:09 +0100

    refactoring

--- src/lib/server/hooks/authorization.ts
+++ src/lib/server/hooks/authorization.ts
@@ -1,53 +1,43 @@
-import {type Handle, redirect} from "@sveltejs/kit";
-import {Roles} from "$types/db";
-import hasRole from "$lib/utils/hasRole";
+import {type Handle, redirect} from '@sveltejs/kit'
+import {Roles} from '$types/db'
+import hasRole from '$lib/utils/hasRole'
 
 // Authorization middleware for route access control
 const authorization: Handle = async ({event, resolve}) => {
-  const user = event.locals.user;
-  const path = event.url.pathname;
+  const user = event.locals.user
+  const path = event.url.pathname
 
-  const publicPaths = ["/", "/contacts", "/login", "/maintenance"];
+  const publicPaths = ['/', '/contacts', '/login', '/maintenance']
 
   // Allow access to public paths without authentication
   if (publicPaths.includes(path)) {
-    return await resolve(event);
+    return await resolve(event)
   }
 
   // Redirect unauthenticated users to the login page
   if (!user) {
-    redirect(302, "/login");
+    redirect(302, '/login')
   }
 
   // Role-based access control mapping for different routes
   const routePermissions = Object.entries({
-    "/cogestione/registration": !hasRole(user, Roles.Staff, Roles.Docente),
-    "/cogestione/ticket": !hasRole(user, Roles.Staff, Roles.Docente),
-    "/cogestione/admin": hasRole(user, Roles.Admin),
-    "/cogestione/classes": hasRole(user, Roles.Rappresentante, Roles.Admin),
-    "/cogestione/activities": hasRole(
-      user,
-      Roles.Organizzatore,
-      Roles.Admin,
-      Roles.Docente
-    ),
-    "/cogestione/security": hasRole(
-      user,
-      Roles.Security,
-      Roles.Staff,
-      Roles.Admin
-    ),
-    "/cogestione/staff": hasRole(user, Roles.Staff, Roles.Admin),
-  });
+    '/cogestione/registration': !hasRole(user, Roles.Staff, Roles.Docente),
+    '/cogestione/ticket': !hasRole(user, Roles.Staff, Roles.Docente),
+    '/cogestione/admin': hasRole(user, Roles.Admin),
+    '/cogestione/classes': hasRole(user, Roles.Rappresentante, Roles.Admin),
+    '/cogestione/activities': hasRole(user, Roles.Organizzatore, Roles.Admin, Roles.Docente),
+    '/cogestione/security': hasRole(user, Roles.Security, Roles.Staff, Roles.Admin),
+    '/cogestione/staff': hasRole(user, Roles.Staff, Roles.Admin),
+  })
 
   // Redirect unauthorized users to the login page
   for (const [route, permission] of routePermissions) {
     if (path.startsWith(route) && !permission) {
-      redirect(302, "/login");
+      redirect(302, '/login')
     }
   }
 
-  return await resolve(event);
-};
+  return await resolve(event)
+}
 
-export default authorization;
+export default authorization
