220b863d2f81795708c573d66b384d8d4abeea9d
branila <stefan.branila55@gmail.com>
2024-12-17 04:02:03 +0100

    ticket page

--- src/routes/cogestione/ticket/+page.server.ts
+++ src/routes/cogestione/ticket/+page.server.ts
@@ -1,7 +1,17 @@
 import type { PageServerLoad } from './$types'
+import { error } from '@sveltejs/kit'
 
 export const load: PageServerLoad = async ({ parent, locals }) => {
+  let [ticketQueryError, ticket] = await goCatch(
+    locals.pb.collection('tickets').getFirstListItem(`user="${locals.user?.id}"`)
+  )
+
+  if (ticketQueryError || !ticket) {
+    ticket = undefined
+  }
+
   return {
-    user: locals.user!
+    user: locals.user!,
+    ticket
   }
 }
