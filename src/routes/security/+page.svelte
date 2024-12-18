a502b2fb9bf86f6ff7250c722851e7f20e910a94
branila <stefan.branila55@gmail.com>
2024-12-19 00:53:18 +0100

    yah bruh are we gonna sleep tonight?

--- src/routes/security/+page.svelte
+++ src/routes/security/+page.svelte
@@ -55,13 +55,11 @@
             return
         }
 
-        if (!response) {
+        if (!response || response?.ticket.authenticator) {
           popupColor = 'var(--red)'
         }
 
-        if (response?.ticket.authenticator) {
-          popupColor = 'var(--red)'
-        }
+        popupColor = 'var(--green)'
     }
 
     function markPresence() {
@@ -157,7 +155,7 @@
                 </div>
 
                 <div class=".popup-btn">
-                    {#if !response.ticket.authenticator}
+                    {#if response.ticket.authenticator}
                         <SimpleButton onclick={markPresence}>Segna presente</SimpleButton>
                     {/if}
                 </div>
