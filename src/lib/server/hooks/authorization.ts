5ea5dc04ad7d76c27a00b073ed4057d1b77a7204
branila <stefan.branila55@gmail.com>
2024-12-20 23:29:11 +0100

    tac tic

--- src/lib/server/hooks/authorization.ts
+++ src/lib/server/hooks/authorization.ts
@@ -31,7 +31,7 @@ const authorization: Handle = async ({event, resolve}) => {
     '/cogestione/admin': hasRole(user, Roles.Admin),
     '/cogestione/classes': hasRole(user, Roles.Rappresentante, Roles.Admin),
     '/cogestione/activities': hasRole(user, Roles.Organizzatore, Roles.Admin, Roles.Docente),
-    '/security': hasRole(user, Roles.Security, Roles.Staff, Roles.Admin),
+    '/security': hasRole(user, Roles.Security, Roles.Staff, Roles.Admin, Roles.Docente),
     '/cogestione/staff': hasRole(user, Roles.Staff, Roles.Admin),
   })
 
