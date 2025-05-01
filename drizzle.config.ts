668bdfdc91d527f1457473bae4b52bbe091a731d
branila <stefan.branila55@gmail.com>
2025-05-01 12:25:26 +0200

    migration from pocketbase to postgresql and drizzle: basic configuration

---
+++ drizzle.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'drizzle-kit'
+
+if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
+
+export default defineConfig({
+	dbCredentials: {
+		url: process.env.DATABASE_URL
+	},
+	schema: './src/lib/server/db/schema.ts',
+	out: './src/lib/server/db/migrations',
+	dialect: 'postgresql',
+	verbose: true,
+	strict: true,
+})
