451e923817f795c8105593435f51d76af9a33e78
branila <stefan.branila55@gmail.com>
2025-05-01 12:57:22 +0200

    migration from pocketbase to postgresql and drizzle: defined utility function for unique ticket identifiers generation

---
+++ src/lib/server/db/utils.ts
@@ -0,0 +1,20 @@
+import { eq } from 'drizzle-orm'
+import { tickets } from './schema'
+import { db } from './index'
+
+export async function generateUniqueTicketId() {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ123456789'
+  let id = ''
+
+  for (let i = 0; i < 4; i++) {
+    id += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+
+  while (await db.select().from(tickets).where(eq(tickets.id, id))) {
+    id = Array.from({ length: 4 }, () => {
+      return chars.charAt(Math.floor(Math.random() * chars.length));
+    }).join('');
+  }
+
+  return id;
+}
