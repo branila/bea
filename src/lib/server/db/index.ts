23bcd0d75373070bd111decc9630f06b50b39101
branila <stefan.branila55@gmail.com>
2025-05-01 21:27:38 +0200

    installed postgres library for drizzle and artic library for google oauth

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,9 +1,12 @@
 import { drizzle } from 'drizzle-orm/postgres-js'
+import postgres from 'postgres'
 import * as schema from './schema'
-import { env } from '$env/dynamic/private'
+import { DATABASE_URL } from '$env/static/private'
 
-if (!env.DATABASE_URL) {
+if (!DATABASE_URL) {
   throw new Error('DATABASE_URL is not set')
 }
 
-export const db = drizzle(env.DATABASE_URL, { schema })
+const client = postgres(DATABASE_URL)
+
+export const db = drizzle(client, { schema })
