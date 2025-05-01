850a47bfd68caeaabd71e5d30770ff1dedd5180e
branila <stefan.branila55@gmail.com>
2025-05-01 21:51:39 +0200

    set dynamic callback url for authentication

--- src/lib/server/auth.ts
+++ src/lib/server/auth.ts
@@ -1,8 +1,8 @@
 import { Google } from 'arctic'
-import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from '$env/static/private'
+import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_CALLBACK_URL } from '$env/static/private'
 
 export const google = new Google(
 	GOOGLE_CLIENT_ID,
 	GOOGLE_CLIENT_SECRET,
-	'http://localhost:5000/login/callback' // TODO: dacci un'occhiata che sta cosa in prod non va
+	GOOGLE_CALLBACK_URL
 )
