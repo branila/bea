76e8dee1858b7b9e3279dc50e664c323f03fb176
branila <stefan.branila55@gmail.com>
2025-05-30 15:01:27 +0200

    debugging

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -4,6 +4,15 @@ import { sequence } from '@sveltejs/kit/hooks'
 import authentication from '$lib/server/hooks/authentication'
 import authorization from '$lib/server/hooks/authorization'
 import errorsHandler from '$lib/server/hooks/errors'
+import { eventDays } from '$schema'
+
+import { DATABASE_URL } from '$env/static/private'
+import { db } from '$db'
+
+async function pingTest() {
+  await db.select().from(eventDays)
+  console.log(DATABASE_URL)
+}
 
 export const handle: Handle = sequence(
   authentication, authorization
