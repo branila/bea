0befb639d7717107fa1f48f4969093258a4070fc
branila <stefan.branila55@gmail.com>
2025-06-01 16:57:58 +0200

    removed debugging function pingTest

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -6,15 +6,6 @@ import authorization from '$lib/server/hooks/authorization'
 import errorsHandler from '$lib/server/hooks/errors'
 import { eventDays } from '$schema'
 
-import { DATABASE_URL } from '$env/static/private'
-import { db } from '$db'
-
-async function pingTest() {
-  await db.select().from(eventDays)
-  console.log(DATABASE_URL)
-}
-
-pingTest()
 
 export const handle: Handle = sequence(
   authentication, authorization
