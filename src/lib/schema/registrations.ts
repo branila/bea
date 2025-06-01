0b2e7286b57a91de63e3893da76b0def30f6af7d
branila <stefan.branila55@gmail.com>
2025-06-01 17:10:58 +0200

    created "scans" table

--- src/lib/schema/registrations.ts
+++ src/lib/schema/registrations.ts
@@ -1,5 +1,5 @@
 import { timestamps } from './timestamps'
-import { pgTable, text, timestamp, smallint, varchar, primaryKey } from 'drizzle-orm/pg-core'
+import { pgTable, text, timestamp, smallint, varchar, primaryKey, date } from 'drizzle-orm/pg-core'
 import { relations } from 'drizzle-orm'
 import { users } from './users'
 import { turns } from './activities'
@@ -26,20 +26,31 @@ export const registrationsRelations = relations(registrations, ({ one }) => ({
 export const tickets = pgTable('tickets', {
   id: varchar('id', { length: 4 }).primaryKey().notNull(),
   user: text('user').references(() => users.email).unique().notNull(),
-  scanned: timestamp('scanned'),
-  authenticator: text('authenticator').references(() => users.email),
   ...timestamps
 })
 
 export const ticketsRelations = relations(tickets, ({ one }) => ({
   owner: one(users, {
     fields: [tickets.user],
-    references: [users.email],
-    relationName: 'ticketOwner',
+    references: [users.email]
+  }),
+}))
+
+export const scans = pgTable('scans', {
+  ticket: varchar('ticket', { length: 4 }).references(() => tickets.id).notNull(),
+  date: date('date').notNull(),
+  authenticator: text('authenticator').references(() => users.email),
+}, (table) => [
+  primaryKey({ columns: [table.ticket, table.date] })
+])
+
+export const scansRelations = relations(scans, ({ one }) => ({
+  ticket: one(tickets, {
+    fields: [scans.ticket],
+    references: [tickets.id],
   }),
   authenticator: one(users, {
-    fields: [tickets.authenticator],
+    fields: [scans.authenticator],
     references: [users.email],
-    relationName: 'ticketAuthenticator',
   }),
 }))
