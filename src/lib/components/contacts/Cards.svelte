7a78fa8c52aa02b3474cacb0c3268f0b6d0289f5
branila <contact@branila.it>
2026-06-04 02:59:48 +0200

    fix: update contact email to cogestione@itispaleocapa.it

--- src/lib/components/contacts/Cards.svelte
+++ src/lib/components/contacts/Cards.svelte
@@ -7,8 +7,8 @@
         <Card
             title="Problemi"
             description="Segnala problemi o malfunzionamenti della piattaforma. Rispondiamo in ordine di gravità, dando la precedenza alle catastrofi."
-            contact="bea@branila.it"
-            href="mailto:bea@branila.it"
+            contact="cogestione@itispaleocapa.it"
+            href="mailto:cogestione@itispaleocapa.it"
             src="/images/contacts/alert.svg"
         />
 
